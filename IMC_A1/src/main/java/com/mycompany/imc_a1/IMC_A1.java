2a4d20084aad373cb0241888197682c4f096fd03
AlexGarH1 <hdz_619@hotmail.com>
2025-11-30 00:52:31 -0600

    Se agregan archivos al repositorio

---
+++ IMC_A1/src/main/java/com/mycompany/imc_a1/IMC_A1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.imc_a1;
+
+/**
+ *
+ * @author hdz_6
+ */
+public class IMC_A1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
